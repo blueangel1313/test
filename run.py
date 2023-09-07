ce3b1ddaf620519467d7b7d4561070b81a2c2a93
ba <94883769+blueangel1313@users.noreply.github.com>
2023-09-07 15:04:11 +0300

    t

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ stub = modal.Stub("test")
 volume = modal.NetworkFileSystem.new().persisted("test")
 
 @stub.function(
-    modal.Image.from_registry("nvidia/cuda:12.2.0-base-ubuntu22.04", add_python="3.11")
+    modal.Image.from_registry("nvidia/cuda:12.2.0-devel-ubuntu22.04", add_python="3.11")
     .run_commands(
         "apt update -y && \
         apt install -y software-properties-common && \
@@ -27,15 +27,19 @@ volume = modal.NetworkFileSystem.new().persisted("test")
     .pip_install_from_requirements(
         "requirements.txt"
     ),
-    network_file_systems={"/content/test": volume},
+    network_file_systems={"/content": volume},
     gpu="A10G",
     timeout=60000,
 )
 async def run():
-    os.system(f"git clone -b dev https://github.com/camenduru/One-2-3-45-hf /content/test")
+    # os.system(f"!rm -rf /content/test")
+    # os.system(f"git lfs install")
+    # os.system(f"git clone -b dev https://github.com/camenduru/One-2-3-45-hf /content/test")
     os.chdir(f"/content/test")
-    os.environ['HF_HOME'] = '/content/huggingface'
-    os.system(f"python app.py")
+    os.system(f"pip install git+https://github.com/mit-han-lab/torchsparse@v1.4.0")
+    os.system(f"pip show torchsparse")
+    # os.environ['HF_HOME'] = '/content/cache/huggingface'
+    # os.system(f"python app.py")
 
 @stub.local_entrypoint()
 def main():
