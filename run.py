4db7cd5c614e52f93cfb29de79f9b1bb11a66614
ba <94883769+blueangel1313@users.noreply.github.com>
2023-09-07 16:27:07 +0300

    🧹

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ stub = modal.Stub("test")
 volume = modal.NetworkFileSystem.new().persisted("test")
 
 @stub.function(
-    modal.Image.from_registry("nvidia/cuda:11.8.0-devel-ubuntu22.04", add_python="3.11")
+    modal.Image.from_registry("nvidia/cuda:11.8.0-devel-ubuntu22.04", add_python="3.10")
     .run_commands(
         "apt update -y && \
         apt install -y software-properties-common && \
@@ -32,27 +32,10 @@ volume = modal.NetworkFileSystem.new().persisted("test")
     timeout=60000,
 )
 async def run():
-    import pip
-    # pip.main(['install', elev_est_dir])
-    # export TORCH_CUDA_ARCH_LIST="7.0;7.2;8.0;8.6"
-    # export IABN_FORCE_CUDA=1
-    # os.environ["TORCH_CUDA_ARCH_LIST"] = "8.0;8.6"
-    # os.environ["IABN_FORCE_CUDA"] = "1"
-    # os.environ["FORCE_CUDA"] = "1"
-    # pip.main(["install", "inplace_abn"]) 
-    # FORCE_CUDA=1 pip install --no-cache-dir git+https://github.com/mit-han-lab/torchsparse.git@v1.4.0
-    # pip.main(["install", "--no-cache-dir", "git+https://github.com/mit-han-lab/torchsparse.git@v1.4.0"])
-
-    # os.system(f"pip show torch torchvision albumentations opencv-python pudb imageio imageio-ffmpeg pytorch-lightning omegaconf test-tube streamlit einops torch-fidelity transformers kornia webdataset torchmetrics fire gradio diffusers datasets[vision] rich plotly dl_ext loguru matplotlib Pillow PyYAML scikit_image scikit_learn scipy setuptools tensorboardX tqdm transforms3d trimesh yacs gdown onnxruntime  onnx rembg pyhocon icecream PyMCubes ninja")
-    # os.system(f"!rm -rf /content/test")
-    # os.system(f"git lfs install")
-    # os.system(f"git clone -b dev https://github.com/camenduru/One-2-3-45-hf /content/test")
-    # os.chdir(f"/content/test")
-    # os.system(f"ls -al")
-    os.system(f"pip install git+https://github.com/mit-han-lab/torchsparse@v1.4.0")
-    os.system(f"pip show torchsparse")
-    # os.environ['HF_HOME'] = '/content/cache/huggingface'
-    # os.system(f"python app.py")
+    os.system(f"git clone -b dev https://github.com/camenduru/One-2-3-45-hf /content/test")
+    os.chdir(f"/content/test")
+    os.environ['HF_HOME'] = '/content/cache/huggingface'
+    os.system(f"python app.py")
 
 @stub.local_entrypoint()
 def main():
