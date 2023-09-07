f6b512b92cf3a0953bc65af55bf88a0167c9c412
ba <94883769+blueangel1313@users.noreply.github.com>
2023-09-07 13:52:07 +0300

    test

---
+++ run.py
@@ -0,0 +1,36 @@
+import modal
+import os
+
+stub = modal.Stub("test")
+volume = modal.NetworkFileSystem.new().persisted("test")
+
+@stub.function(
+    modal.Image.from_registry("nvidia/cuda:12.2.0-base-ubuntu22.04", add_python="3.11")
+    .pip_install_from_requirements(
+        "requirements.txt"
+    )
+    .run_commands(
+        "apt update -y && \
+        apt install -y software-properties-common && \
+        apt update -y && \
+        add-apt-repository -y ppa:git-core/ppa && \
+        apt update -y && \
+        apt install -y git git-lfs && \
+        git --version  && \
+        apt install -y aria2 libgl1 libglib2.0-0 wget libsparsehash-dev && \
+        pip install -q torch==2.0.1+cu118 torchvision==0.15.2+cu118 torchaudio==2.0.2+cu118 torchtext==0.15.2 torchdata==0.6.1 --extra-index-url https://download.pytorch.org/whl/cu118 && \
+        pip install -q xformers==0.0.20 triton==2.0.0 packaging==23.1"
+    ),
+    network_file_systems={"/content/test": volume},
+    gpu="A10G",
+    timeout=60000,
+)
+async def run():
+    os.system(f"git clone -b dev https://github.com/camenduru/One-2-3-45-hf /content/test")
+    os.chdir(f"/content/test")
+    os.environ['HF_HOME'] = '/content/huggingface'
+    os.system(f"python app.py")
+
+@stub.local_entrypoint()
+def main():
+    run.call()
