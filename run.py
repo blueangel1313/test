1de4d190f0c0dc494d06d9cddf8732a879b90e64
ba <94883769+blueangel1313@users.noreply.github.com>
2023-09-08 08:18:57 +0300

    test

--- run.py
+++ run.py
@@ -31,13 +31,14 @@ volume = modal.NetworkFileSystem.new().persisted("test")
     timeout=60000,
 )
 async def run():
-    os.system(f"git lfs install")
-    os.system(f"git clone https://huggingface.co/spaces/camenduru/One-2-3-45 /content/test")
-    os.chdir(f"/content/test")
-    os.system(f"git pull")
-    os.environ['HF_HOME'] = '/content/cache/huggingface'
     os.system(f"pip install taming-transformers-rom1504")
-    os.system(f"python app.py")
+    os.environ['HF_HOME'] = '/content/cache/huggingface'
+    os.system(f"git lfs install")
+    os.system(f"git clone -b dev https://github.com/camenduru/One-2-3-45 /content/One-2-3-45")
+    os.chdir(f"/content/One-2-3-45")
+    os.system(f"git pull")
+    os.system(f"python run.py --img_path /content/One-2-3-45/demo/demo_examples/00_zero123_lysol.png --half_precision")
+    os.system(f"ls -la /content/One-2-3-45/exp/00_zero123_lysol/mesh.ply")
 
 @stub.local_entrypoint()
 def main():
